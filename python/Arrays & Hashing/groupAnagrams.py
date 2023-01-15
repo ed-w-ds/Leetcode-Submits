c52116f12a3cc1635a586b5a7d77fd9706cf14c1
Eduards Lisovskis <113637889+ed-w-ds@users.noreply.github.com>
2023-01-15 11:28:35 +0000

    Create groupAnagrams.py

---
+++ python/Arrays & Hashing/groupAnagrams.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def groupAnagrams(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: List[List[str]]
+        """
+    # a defaultdict is a dict subclass that calls a factory function (in this case, list) to supply missing values. This means that if you try to access a key that does not exist in the dictionary, it will automatically create that key with a default value (in this case, an empty list) rather than raising a KeyError.
+        results = defaultdict(list) # val : index
+        
+        for s in strs:
+            count = [0] * 26
+            for c in s:
+                # counting how many of each character we have
+               count[ord(c) - ord("a")] += 1
+            # append the string, group all anagrams of the particular count together
+            # lists cannot be keys, so change it to tuple, as tuples are non-mutable (can't be changed)
+            results[tuple(count)].append(s)
+        # returns just the values not the keys
+        return results.values()
