2d6840b7cd4c5ab8b8535a446fe1cec02de4fb53
Eduards Lisovskis <113637889+ed-w-ds@users.noreply.github.com>
2023-01-19 13:06:42 +0000

    Update validParentheses.py

--- python/stack/validParentheses.py
+++ python/stack/validParentheses.py
@@ -1,1 +1,21 @@
 # time: O() | space: O()
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        stack = []
+        hashmap = {'}' : '{', ')' : '(', ']' : '['}
+        if len(s)%2 != 0:
+            return False
+        else: 
+            for c in s:
+                if c in hashmap:
+                    if stack and stack[-1] == hashmap[c]:
+                        stack.pop()
+                    else:
+                        return False
+                else:
+                    stack.append(c)
+            return True if not stack else False
