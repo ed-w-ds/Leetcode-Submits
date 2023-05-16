88505a2deb4f79dd79a03156a4594f0f82e0d942
Eduards Lisovskis <113637889+ed-w-ds@users.noreply.github.com>
2023-05-16 10:47:46 +0100

    Update validPalindrome.py

--- python/twoPointers/validPalindrome.py
+++ python/twoPointers/validPalindrome.py
@@ -22,3 +22,19 @@ class Solution(object):
         return (ord("A") <= ord(c) <= ord("Z") or
         ord("a") <= ord(c) <= ord("z") or
         ord("0") <= ord(c) <= ord("9"))
+    
+    # alternative solution using built in python functions
+    class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        l = 0
+        r = len(s) - 1
+        while l < r:
+            while l < r and not s[l].isalnum():
+                l += 1
+            while r > l and not s[r].isalnum():
+                r -= 1
+            if s[l].lower() != s[r].lower():
+                return False
+            l += 1
+            r -= 1
+        return True
