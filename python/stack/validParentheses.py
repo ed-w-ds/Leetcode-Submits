6a2bc9413d3a1c98c6a2c12c15a8aaa0e8f321e4
Eduards Lisovskis <113637889+ed-w-ds@users.noreply.github.com>
2023-05-28 11:41:47 +0100

    Update validParentheses.py

--- python/stack/validParentheses.py
+++ python/stack/validParentheses.py
@@ -19,3 +19,21 @@ class Solution(object):
                 else:
                     stack.append(c)
             return True if not stack else False
+# python 3
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        parentheses = {")" : "(", "}" : "{", "]" : "["}
+
+        if len(s) % 2 != 0:
+            return False
+        else:
+            for p in s:
+                if p in parentheses:
+                    if stack and stack[-1] == parentheses[p]:
+                        stack.pop()
+                    else:
+                        return False
+                else:
+                    stack.append(p)
+            return not stack
