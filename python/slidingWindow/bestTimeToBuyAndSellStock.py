bba6fc44f0ca82fda55e706cb743c39a8e55eb4f
Eduards Lisovskis <113637889+ed-w-ds@users.noreply.github.com>
2023-06-11 10:32:05 +0100

    Update bestTimeToBuyAndSellStock.py

--- python/slidingWindow/bestTimeToBuyAndSellStock.py
+++ python/slidingWindow/bestTimeToBuyAndSellStock.py
@@ -1,13 +1,11 @@
 # time: O(n) | Space: O(1)
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        l, r = 0, 1
-        max_profit = 0
-        while r < len(prices):
-            if prices[l] < prices[r]:
-                profit = prices[r] - prices[l]
-                max_profit = max(profit, max_profit)
-            else:
+        l, maxProfit  = 0, 0
+        for r in range(len(prices)):
+            if prices[l] >= prices[r]:
                 l = r
-            r += 1
-        return max_profit
+            else:
+                profit = prices[r] - prices[l]
+                maxProfit = max(maxProfit, profit)
+        return maxProfit
