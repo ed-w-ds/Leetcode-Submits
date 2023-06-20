cf9186fecbaea1d1a3888e911e4795dd5b4ba228
Eduards Lisovskis <113637889+ed-w-ds@users.noreply.github.com>
2023-06-20 15:15:06 +0100

    Update reverseLinkedList.py

--- python/Linked List/reverseLinkedList.py
+++ python/Linked List/reverseLinkedList.py
@@ -1,9 +1,10 @@
-# Time: O(n) | Space: O(1)
+
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+# Iterative - Time: O(n) | Space: O(1)
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         current = head
