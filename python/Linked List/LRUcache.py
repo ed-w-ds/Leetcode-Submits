b782f4ede54e5f2c207e7ca7c0528beaa22c836e
Eduards Lisovskis <113637889+ed-w-ds@users.noreply.github.com>
2023-06-28 10:41:28 +0100

    Create LRUcache.py

---
+++ python/Linked List/LRUcache.py
@@ -0,0 +1,52 @@
+# class for nodes
+class Node:
+    def __init__(self, key, val):
+        self.key, self.val = key, val
+        self.prev = self.next = None
+
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.cache = {}
+        self.cap = capacity
+
+        # LRU = left and MRU = right (least recently used and most recently used)
+        self.left = Node(0, 0)
+        self.right = Node(0, 0)
+        self.left.next = self.right
+        self.right.prev = self.left
+
+    # insert O(1)
+    def insert(self, node):
+        prev, nxt = self.right.prev, self.right
+        prev.next = nxt.prev = node
+        node.prev, node.next = prev, nxt
+
+    # remove O(1)
+    def remove(self, node):
+        prev, nxt = node.prev, node.next
+        nxt.prev, prev.next = prev, nxt
+
+    def get(self, key: int) -> int:
+        if key in self.cache:
+            self.remove(self.cache[key])
+            self.insert(self.cache[key])
+            return self.cache[key].val
+        return -1
+        
+    def put(self, key: int, value: int) -> None:
+        if key in self.cache:
+            self.remove(self.cache[key])
+        self.cache[key] = Node(key, value)
+        self.insert(self.cache[key])
+
+        if len(self.cache) > self.cap:
+            lru = self.left.next
+            self.remove(lru)
+            del self.cache[lru.key]
+
+
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
